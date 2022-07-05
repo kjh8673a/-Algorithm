148104cf417928b5afbd9d21ec7b611006d3144a
Jihoon Gweon <kjh8673a@naver.com>
2022-07-05 10:02:35 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 272 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt()*2 + 2*sc.nextInt()*3.141592);
+
+        sc.close();
+    }
+
+}
