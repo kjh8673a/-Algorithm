2621958dcb26e61a79c53b319e1d83129bbe3448
Jihoon Gweon <kjh8673a@naver.com>
2022-07-13 18:29:04 +0900

    [Bronze III] Title: 세 막대, Time: 252 ms, Memory: 17840 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {       
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+        sc.close();
+
+        Arrays.sort(arr);
+
+        if(arr[0] + arr[1] > arr[2]) {
+            System.out.println(arr[0] + arr[1] + arr[2]);
+        }else{
+            arr[2] = arr[0] + arr[1] - 1;
+            System.out.println(arr[0] + arr[1] + arr[2]);
+        }
+
+    }
+}
