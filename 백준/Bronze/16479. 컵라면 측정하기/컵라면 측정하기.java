6eca981d4f29fb6db6a3ad013ba1dae1e9cecf6f
Jihoon Gweon <kjh8673a@naver.com>
2022-07-16 00:30:41 +0900

    [Bronze III] Title: 컵라면 측정하기, Time: 232 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Bronze/16479. 컵라면 측정하기/컵라면 측정하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {       
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double k = sc.nextDouble();
+        double d1 = sc.nextDouble();
+        double d2 = sc.nextDouble();
+        sc.close();
+        System.out.println(Math.pow(k, 2) - Math.pow((d1-d2)/2,2));
+    }
+}
