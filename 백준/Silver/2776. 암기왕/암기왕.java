a69c1d0377d72f5dbdfd9398b53d13682324aac5
Jihoon Gweon <kjh8673a@naver.com>
2022-08-09 23:38:09 +0900

    [Silver IV] Title: 암기왕, Time: 1564 ms, Memory: 281392 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,49 +1,39 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        int t = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < t; i++) {
-            st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-
-            int[] arr = new int[n];
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-            Arrays.sort(arr);
-
-            st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                int check = Integer.parseInt(st.nextToken());
-                int ans = binarySearch(arr, 0, arr.length - 1, check);
-                sb.append(String.valueOf(ans) + "\n");
-            }
-
-        }
-        System.out.println(sb.toString());
-
-    }
-
-    public static int binarySearch(int[] a, int start, int end, int key) {
-        if (start > end) return 0;
-
-        int mid = (start + end) / 2;
-        if (a[mid] > key) return binarySearch(a, start, mid - 1, key);
-        else if (a[mid] < key) return binarySearch(a, mid + 1, end, key);
-        else return 1;
-    }
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int tc = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= tc; i++) {
+			Set<Integer> memo1 = new HashSet<>();
+			int su1 = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int a = 0; a < su1; a++) {
+				int N = Integer.parseInt(st.nextToken());
+				memo1.add(N);
+			}
+			int su2 = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for(int a=0; a<su2; a++) {
+				if(memo1.contains(Integer.parseInt(st.nextToken()))) {
+					bw.write('1' + "\n");
+				}else {
+					bw.write('0' + "\n");
+				}
+			}	
+		}
+		
+        bw.flush();
+	}
 }
