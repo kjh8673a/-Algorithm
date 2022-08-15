7a6c1f6e5ec87abe89c342ab1863be41e85cfe3f
Jihoon Gweon <kjh8673a@naver.com>
2022-08-15 22:21:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 72 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,29 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt(); 
-        sc.close();
-        
-        int f = n / 5; 
-        int t = (n % 5) / 3;
+        int n = Integer.parseInt(br.readLine());
 
-        if ((n % 5) % 3 != 0) {
-            for(int i = n/5; i >= 0; i--) {
+        // 5킬로그램 봉지 개수
+        int five = n / 5;
+
+        // 3킬로그램 봉지 개수
+        int three = (n % 5) / 3;
+
+
+        // n을 5로 나눈 나머지가 3으로 나누어 떨어지면 그게 베스트
+        // 3으로 나누어떨어지지 않으면...
+        if((n % 5) % 3 != 0) {
+            // 5킬로그램 봉지를 최대에서 하나씩 줄여가면서 3으로 나눠본다
+            for(int i = n / 5; i >= 0; i--) {
                 if((n-5*i) % 3 == 0) {
-                    f = i;
-                    t = (n-5*i) / 3;
+                    five = i;
+                    three = (n-5*i) / 3;
                     break;
-                } else {
-                    f = 0;
-                    t = -1;
+                }else {
+                    five = 0;
+                    three = -1;
                 }
-            }    
-        } 
-        
-        System.out.println(f + t);
-        
+            }
+        }
+
+        System.out.println(five + three);
+
     }
+
 }
