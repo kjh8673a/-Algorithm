9af6743f04df84d87126eaf9ba867e846de6fa03
Jihoon Gweon <kjh8673a@naver.com>
2022-08-17 00:14:18 +0900

    [Silver IV] Title: 큐 2, Time: 1444 ms, Memory: 350952 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static Queue<Integer> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        queue = new LinkedList<>();
+
+        int num = 0;
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String key = st.nextToken();
+            
+            switch(key) {
+                case "push" :
+                    num = Integer.parseInt(st.nextToken());
+                    queue.offer(num);
+                    break;
+                case "pop" :
+                    if(queue.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(queue.poll()).append("\n");
+                    break;
+                case "size" :
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty" :
+                    if(queue.isEmpty()) sb.append(1).append("\n");
+                    else sb.append(0).append("\n");
+                    break;
+                case "front" :
+                    if(queue.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(queue.peek()).append("\n");
+                    break;
+                case "back" :
+                    if(queue.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(num).append("\n");
+                    break;
+            }
+
+        }
+
+        System.out.println(sb.toString());
+        
+    }
+}
