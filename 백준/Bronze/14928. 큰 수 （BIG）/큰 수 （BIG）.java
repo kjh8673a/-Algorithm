78103e60c14c30f97643fe03cb0e14d4b74ac09f
Jihoon Gweon <kjh8673a@naver.com>
2022-08-16 20:02:07 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 260 ms, Memory: 20100 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String num = br.readLine();
+        long remainder = 0;
+        for (int i = 0; i < num.length(); i++) {
+            remainder = (remainder * 10 + num.charAt(i) - '0') % 20000303;
+        }
+
+        System.out.println(remainder);
+
+    }
+
+}
