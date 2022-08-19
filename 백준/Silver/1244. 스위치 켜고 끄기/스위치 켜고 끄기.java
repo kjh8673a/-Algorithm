dfe7265061e6cbab3b13a41d0a38a3fa036796a4
Jihoon Gweon <kjh8673a@naver.com>
2022-08-19 11:22:22 +0900

    [Silver III] Title: 스위치 켜고 끄기, Time: 144 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -4,60 +4,68 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
+        int[] swit = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            swit[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(br.readLine());
 
-        
         for(int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int mf = Integer.parseInt(st.nextToken());
-            int loc = Integer.parseInt(st.nextToken());
+            int gender = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
 
-            switch(mf) {
-                case 1:
-                    for(int j = 0; j < n; j++) {
-                        if((j+1) % loc == 0) {
-                            arr[j] = (arr[j] == 0) ? 1 : 0;
-                        }
+            // 남학생 -> num의 배수 스위치 바꾸기
+            if(gender == 1) {
+                for(int j = 0; j < n; j++) {
+                    if((j+1) % num == 0) {
+                       swit[j] = (swit[j] == 0) ? 1 : 0;
                     }
-                    break;
-                case 2:
-                    arr[loc-1] = (arr[loc-1] == 0) ? 1 : 0;
-                    for(int j = 1; j <= Math.min(n-loc, loc-1); j++) {
-                        if(arr[loc-1+j] == arr[loc-1-j]) {
-                            arr[loc-1+j] = (arr[loc-1+j] == 0) ? 1 : 0;
-                            arr[loc-1-j] = (arr[loc-1-j] == 0) ? 1 : 0;
-                        }else {
-                            break;
-                        }
+                }
+            }
+
+            // 여학생 -> num부터 좌우로 하나씩 가면서 대칭인지 확인 후 바꾸기
+            if(gender == 2) {
+                // 일단 주어진 num에 해당하는 스위치 바꾸고 시작
+                swit[num-1] = (swit[num-1] == 0) ? 1 : 0;
+
+                // num에서 한칸 오른쪽부터 시작하여 대칭인 놈 체크
+                for(int j = num; j < n; j++) {
+
+                    // j와 주어진 num에 해당하는 스위치의 차이 -> j - 2*a가 j와 대칭인 놈
+                    int a = j - (num - 1);
+
+                    if(j - a * 2 < 0) break;
+
+                    if(swit[j] == swit[j-a*2]) {
+                        swit[j] = (swit[j] == 0) ? 1 : 0;
+                        swit[j-a*2] = (swit[j-a*2] == 0) ? 1 : 0;
+                    }else {
+                        break;
                     }
+                }
             }
         }
 
-        int cnt = 0;
         for(int i = 0; i < n; i++) {
-            sb.append(arr[i] + " ");
-            cnt++;
-            if(cnt % 20 == 0 && i != 0) {
+            sb.append(swit[i] + " ");
+            if((i+1) % 20 == 0 && i != 0) {
                 sb.append("\n");
             }
         }
 
         System.out.println(sb.toString());
-        
+
     }
 
 }
