939ef23dbd52cb0ccb42672904a9e848a7b414a8
Jihoon Gweon <kjh8673a@naver.com>
2022-08-19 20:47:33 +0900

    [Silver II] Title: 조합 0의 개수, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Integer.parseInt(st.nextToken());
+        long m = Integer.parseInt(st.nextToken());
+
+        long cnt5 = pow5(n) - pow5(m) - pow5(n - m);
+        long cnt2 = pow2(n) - pow2(m) - pow2(n - m);
+
+        System.out.println(Math.min(cnt5, cnt2));
+    }
+
+    public static long pow5(long num) {
+        int cnt = 0;
+
+        while (num >= 5) {
+            cnt += (num / 5);
+            num /= 5;
+        }
+
+        return cnt;
+    }
+
+    public static long pow2(long num) {
+        int cnt = 0;
+
+        while (num >= 2) {
+            cnt += (num / 2);
+            num /= 2;
+        }
+
+        return cnt;
+    }
+
+}
