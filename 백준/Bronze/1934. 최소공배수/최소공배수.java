af5727fbc64bf39137ae2950a2af661536291247
Jihoon Gweon <kjh8673a@naver.com>
2022-08-19 14:49:55 +0900

    [Bronze I] Title: 최소공배수, Time: 596 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < t; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            Map<Integer, Integer> mapA = new HashMap<>();
+            while (a > 1) {
+                for (int i = 2; i <= a; i++) {
+                    if (a % i == 0) {
+                        mapA.put(i, mapA.getOrDefault(i, 0) + 1);
+                        a /= i;
+                        break;
+                    }
+                }
+            }
+
+            Map<Integer, Integer> mapB = new HashMap<>();
+            while (b > 1) {
+                for (int i = 2; i <= b; i++) {
+                    if (b % i == 0) {
+                        mapB.put(i, mapB.getOrDefault(i, 0) + 1);
+                        b /= i;
+                        break;
+                    }
+                }
+            }
+
+            int big = 1;
+
+            for (int nKey : mapA.keySet()) {
+                if (mapB.containsKey(nKey)) {
+                    big *= Math.pow(nKey, Math.max(mapA.get(nKey), mapB.get(nKey)));
+                } else {
+                    big *= Math.pow(nKey, mapA.get(nKey));
+                }
+            }
+
+            for (int nKey : mapB.keySet()) {
+                if (!mapA.containsKey(nKey)) {
+                    big *= Math.pow(nKey, mapB.get(nKey));
+                }
+            }
+
+            sb.append(big).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
+}
