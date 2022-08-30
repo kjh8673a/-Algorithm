452dbe6e2bf510a33b1baeda83c13be5b2cc8e11
Jihoon Gweon <kjh8673a@naver.com>
2022-08-30 09:46:43 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,29 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
 
-        int[] num = new int[str.length()];
+        int[] numbers = new int[10]; // 각 자리수(0~9) 카운트할 배열
 
         for(int i = 0; i < str.length(); i++) {
-            num[i] = Character.getNumericValue(str.charAt(i));
+            int num = str.charAt(i) - '0';
+            numbers[num]++;
+        }
+        
+        // 9부터 내려오면서 numbers배열의 개수만큼 해당 번호 출력
+        for(int i = 9; i >= 0; i--) {
+            while(numbers[i]-- > 0) {
+                sb.append(i);
+            }
         }
 
-        Arrays.sort(num);
-
-        for(int i = num.length-1; i >= 0; i--) {
-            sb.append(num[i]);
-        }
-
-        System.out.println(sb.toString());
-
+        System.out.println(sb);
     }
 
 }
