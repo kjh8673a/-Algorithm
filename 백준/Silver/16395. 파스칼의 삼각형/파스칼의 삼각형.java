e110d7fd6d4531fe6bc98c2a198c290604ed7aaf
Jihoon Gweon <kjh8673a@naver.com>
2022-09-01 16:35:47 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,21 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-        public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int[][] pascal;
+    static int n;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        sc.close();
+        n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 
-        System.out.println(comb(n-1,k-1));
+        pascal = new int[n][];
+        makePascal();
+
+        System.out.println(pascal[n-1][k-1]);
+
     }
 
-    public static int comb(int n, int k) {
-        if(n == k || k == 0) {
-            return 1;
-        }else {
-            return comb(n-1, k-1) + comb(n-1, k);
+    public static void makePascal() {
+        for(int i = 0; i < n; i++) {
+            pascal[i] = new int[i+1];
+            pascal[i][0] = 1;
+            pascal[i][i] = 1;
+
+            if(i >= 2) {
+                for(int j = 1; j < i; j++) {
+                    pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
+                }
+            }
         }
     }
+
 }
