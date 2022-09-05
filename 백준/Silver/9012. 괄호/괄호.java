c890c4d9ac475da849da2a407e731db35f384893
Jihoon Gweon <kjh8673a@naver.com>
2022-09-05 20:36:48 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int t = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < t; i++) {
-            String str = sc.next();
+        for(int i = 0; i < n; i++) {
+            String str = br.readLine();
 
-            String ans = "YES";
-            int check = 0;
+            Stack<Character> stack = new Stack<>();
             for(int j = 0; j < str.length(); j++) {
-                if(str.charAt(j) == '(') {
-                    check++;
-                }else if(str.charAt(j) == ')') {
-                    check--;
-                }
-                if(check < 0) {
-                    ans = "NO";
+                stack.add(str.charAt(j));
+            }
+
+            int cnt = 0;
+            boolean isOk = true;
+            while(!stack.isEmpty()) {
+                char ch = stack.pop();
+
+                if(ch == ')') cnt++;
+                if(ch == '(') cnt--;
+
+                if(cnt < 0) {
+                    isOk = false;
                     break;
                 }
             }
-            if(check != 0) {
-                ans = "NO";
+            if(cnt != 0) {
+                isOk = false;
             }
 
-            System.out.println(ans);
+            if(isOk) {
+                sb.append("YES").append("\n");
+            }else {
+                sb.append("NO").append("\n");
+            }
         }
-        
-        
-        sc.close();
+
+        System.out.println(sb.toString());
 
     }
+
 }
