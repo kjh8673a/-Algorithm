844eed04a5380deaaedc6107a3f6783aa1e4983e
Jihoon Gweon <kjh8673a@naver.com>
2022-09-17 23:28:03 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		long[] dp = new long[1001];
+		dp[1] = 1;
+		dp[2] = 3;
+		for (int i = 3; i < 1001; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+		}
+
+		int n = Integer.parseInt(br.readLine());
+
+		System.out.println(dp[n]);
+
+	}
+
+}
