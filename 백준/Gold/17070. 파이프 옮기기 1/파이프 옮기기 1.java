c00ef4c6ec03659bb4440ecfbefb575c5437e8ac
Jihoon Gweon <kjh8673a@naver.com>
2022-09-20 14:43:17 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 220 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -9,7 +9,6 @@ public class Main {
     static int cnt;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(br.readLine());
 
@@ -24,6 +23,11 @@ public class Main {
 
         cnt = 0;
 
+		// 파이프의 끝이 위치한 좌표를 (a,b)로 한다.
+		// loc는 파이프가 놓여진 모양을 나타낸다.
+		// 1 - 가로
+		// 2 - 대각선
+		// 3 - 세로
         solve(0, 1, 1);
 
         System.out.println(cnt);
@@ -31,11 +35,14 @@ public class Main {
     }
 
     public static void solve(int a, int b, int loc) {
+		// 파이프의 끝점이 (n-1, n-1)에 위치했을때 종료한다
+		// 이차원배열을 [n][n]으로 선언했기때문에 (n-1, n-1)이 종료지점.
         if(a == n-1 && b == n-1) {
             cnt++;
             return;
         }
-        
+
+        // 가로
         if(loc == 1 && b+1 < n && board[a][b+1] == 0) {
             solve(a, b+1, 1);
         }
@@ -43,6 +50,7 @@ public class Main {
             solve(a+1, b+1, 2);
         }
 
+		// 대각선
         if(loc == 2 && b+1 < n && board[a][b+1] == 0) {
             solve(a, b+1, 1);
         }
@@ -53,6 +61,7 @@ public class Main {
             solve(a+1, b+1, 2);
         }
 
+		// 세로
         if(loc == 3 && a+1 < n && board[a+1][b] == 0) {
             solve(a+1, b, 3);
         }
