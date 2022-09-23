24469cd3af4accd1ae2a4cf35c85e966d0af06b6
Jihoon Gweon <kjh8673a@naver.com>
2022-09-23 09:06:45 +0900

    [Silver II] Title: 섬의 개수, Time: 176 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,4 +1,7 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[] dr = {0, 0, 1, -1, -1, 1, -1, 1};
@@ -8,23 +11,25 @@ public class Main {
     static int[][] map;
     static int cnt;
     static boolean[][] visit;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while(true) {
-            w = sc.nextInt();
-            h = sc.nextInt();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
 
             if(w == 0 && h == 0) {
-                sc.close();
+				br.close();
                 break;
             }
 
             map = new int[h][w];
 
             for(int i = 0; i < h; i++) {
+				st = new StringTokenizer(br.readLine());
                 for(int j = 0; j < w; j++) {
-                    map[i][j] = sc.nextInt();
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
