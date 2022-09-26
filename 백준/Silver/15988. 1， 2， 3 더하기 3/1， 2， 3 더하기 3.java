55ed38f9c2e8f813b240207a424e0ff615cb2dd0
Jihoon Gweon <kjh8673a@naver.com>
2022-09-27 08:55:42 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 164 ms, Memory: 22168 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static long mod = 1000000009;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		long[] dp = new long[1000001];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for (int i = 4; i < 1000001; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % mod;
+		}
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			sb.append(dp[n]).append("\n");
+		}
+
+		System.out.println(sb.toString());
+
+	}
+
+}
