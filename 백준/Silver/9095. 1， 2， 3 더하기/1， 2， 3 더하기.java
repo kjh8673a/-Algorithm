4a8b49d579d2163a123043bed2112f73540d7854
Jihoon Gweon <kjh8673a@naver.com>
2022-09-27 08:59:52 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -3,28 +3,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
+	static int n, ans;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
-		int[] dp = new int[11];
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 4;
-		for(int i = 4; i < 11; i++) {
-			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-		}
-
 		int t = Integer.parseInt(br.readLine());
 
-		for(int i = 0; i < t; i++) {
-			int n = Integer.parseInt(br.readLine());
-			sb.append(dp[n]).append("\n");
+		while(t-- > 0) {
+			n = Integer.parseInt(br.readLine());
+			ans = 0;
+			makeNum(0);
+
+			sb.append(ans).append("\n");
 		}
 
 		System.out.println(sb.toString());
+	}
 
+	public static void makeNum(int num) {
+		if(num > n) {
+			return;
+		}
+		
+		if(num == n) {
+			ans++;
+			return;
+		}
+
+		makeNum(num + 1);
+		makeNum(num + 2);
+		makeNum(num + 3);
 	}
 
 }
