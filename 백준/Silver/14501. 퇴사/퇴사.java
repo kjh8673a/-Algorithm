d08ee8f401db0d4f60230d0eb5b00696e982fa4a
Jihoon Gweon <kjh8673a@naver.com>
2022-09-27 09:21:31 +0900

    [Silver III] Title: 퇴사, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,36 +1,47 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
+	static int n, ans;
+	static int[][] arr;
 
-        int[] t = new int[n+10];
-        int[] p = new int[n+10];
-        for(int i = 0; i < n; i++) {
-            t[i] = sc.nextInt();
-            p[i] = sc.nextInt();
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        sc.close();
+		n = Integer.parseInt(br.readLine());
 
-        int dp[] = new int[n+10];
-        int max = 0;
+		arr = new int[n + 1][2];
+		for (int i = 1; i < n + 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
 
-        for(int i = 0; i < n; i++) {
-            max = i+t[i];
-            if(max <= n+1) {
-                for(int j = n; j >= max; j--){
-                    if(dp[j]<p[i]+dp[i]){
-                        dp[j]=p[i]+dp[i];
-                    }
-                }
-            }
+		ans = 0;
 
-        }
+		work(1, 0);
 
-        System.out.println(dp[n]);
+		System.out.println(ans);
 
-    }
+	}
+
+	public static void work(int day, int total) {
+		if (day >= n + 1) {
+			ans = Math.max(ans, total);
+			return;
+		}
+
+		// 날짜 + 기간이 n+1보다 작거나 같을때만 일을 할수있다
+		if (day + arr[day][0] <= n + 1) {
+			work(day + arr[day][0], total + arr[day][1]);
+		}
+
+		// 일을 안하고 다음날로 넘어간다
+		work(day + 1, total);
+
+	}
+
 }
