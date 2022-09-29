193f97d8e97f5638772baea62c9e92b0e3d90d60
Jihoon Gweon <kjh8673a@naver.com>
2022-09-29 16:01:44 +0900

    [Gold IV] Title: 전력난, Time: 852 ms, Memory: 247416 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.java
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -1,29 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Edge implements Comparable<Edge> {
-		int v;
-		int weight;
-
-		public Edge(int v, int weight) {
-			this.v = v;
-			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(Edge o){
-			return Integer.compare(this.weight, o.weight);
-		}
-	}
 
 	static int V, E;
-	static List<Edge>[] graph;
+	static long ans;
+	static int[][] graph;
+	static int[] parent;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,10 +25,7 @@ public class Main {
 				break;
 			}
 	
-			graph = new ArrayList[V];
-			for(int i = 0; i < V; i++) {
-				graph[i] = new ArrayList<>();
-			}
+			graph = new int[E][3];
 	
 			long total = 0;
 			for(int i = 0; i < E; i++) {
@@ -50,33 +34,41 @@ public class Main {
 				int b = Integer.parseInt(st.nextToken()); // 도착 정점
 				int c = Integer.parseInt(st.nextToken()); // 가중치
 	
-				graph[a].add(new Edge(b, c));
-				graph[b].add(new Edge(a, c));
+				graph[i][0] = a;
+				graph[i][1] = b;
+				graph[i][2] = c;
 	
 				total += c;
 			}
-	
-			long ans = 0;
-	
-			// 프림 알고리즘
-			boolean[] visit = new boolean[V]; // 방문처리
-	
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
-	
-			visit[0] = true;
-	
-			pq.addAll(graph[0]);
-	
-			int cnt = 1; // 획득한 정점의 개수
-			while(cnt != V) {
-				Edge edge = pq.poll(); // 연결된 간선 하나 뽑아서
-	
-				if(visit[edge.v]) continue; // 이미 뽑은 정점이라면 넘어간다
-	
-				ans += edge.weight;
-				pq.addAll(graph[edge.v]);
-				visit[edge.v] = true;
-				cnt++;
+
+			Arrays.sort(graph, new Comparator<int[]>() {
+				@Override
+				public int compare(int[] o1, int[] o2) {
+					return o1[2] - o2[2];
+				}
+			});
+
+			parent = new int[V];
+			for(int i = 0; i < parent.length; i++) {
+				parent[i] = i;
+			}
+
+			// 크루스칼 알고리즘
+			ans = 0;
+			int pick = 0;
+			for(int i = 0; i < E; i++) {
+				int px = findSet(graph[i][0]);
+				int py = findSet(graph[i][1]);
+
+				if(px != py) {
+					ans += graph[i][2];
+					union(px, py);
+					pick++;
+				}
+
+				if(pick == V-1) {
+					break;
+				}
 			}
 			
 			sb.append(total - ans).append("\n");
@@ -86,5 +78,26 @@ public class Main {
 		System.out.println(sb.toString());
 		
 	}
+
+	public static int findSet(int x) {
+        if(x == parent[x]) {
+            return x;
+        }else {
+            parent[x] = findSet(parent[x]);
+
+            return parent[x];
+        }
+    }
+
+    public static void union(int x, int y) {
+        int px = findSet(x);
+        int py = findSet(y);
+
+        if(px > py) {
+            parent[py] = px;
+        }else {
+            parent[px] = py;
+        }
+    }
 
 }
