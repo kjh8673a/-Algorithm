4bfff89b2fafdd25b5a6c3d695b94fbe51e5151d
Jihoon Gweon <kjh8673a@naver.com>
2022-12-06 14:30:22 +0900

    [Silver I] Title: 계단, Time: 324 ms, Memory: 25584 KB -BaekjoonHub

---
+++ 백준/Silver/21600. 계단/계단.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int ans = 0;
+        int tmp = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num > tmp) {
+                tmp++;
+                if (tmp > ans) {
+                    ans = tmp;
+                }
+            } else {
+                tmp = num;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+}
