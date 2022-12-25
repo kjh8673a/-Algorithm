d0fa03091d57e45fab408f7d4af13cce5e040568
Jihoon Gweon <kjh8673a@naver.com>
2022-12-25 16:23:12 +0900

    [Silver V] Title: 배열 합치기, Time: 2336 ms, Memory: 346368 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			pq.add(Integer.parseInt(st.nextToken()));
+		}
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			pq.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		while(!pq.isEmpty()) {
+			sb.append(pq.poll() + " ");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
