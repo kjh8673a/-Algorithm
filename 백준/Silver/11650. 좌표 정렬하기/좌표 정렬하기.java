714b866933f103f229c29c5d4964dd71124cd0d8
Jihoon Gweon <kjh8673a@naver.com>
2023-02-06 22:52:22 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 768 ms, Memory: 60124 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,37 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st;
-        int[][] arr = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(br.readLine());
+
+        double[] arr = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double a = Integer.parseInt(st.nextToken());
+            double b = Integer.parseInt(st.nextToken());
+
+            arr[i] = a+100000 + (b+100000)/1000000;
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] != o2[0]) {
-                    return Integer.compare(o1[0], o2[0]);
-                } else {
-                    return Integer.compare(o1[1], o2[1]);
-                }
-            }
-        });
+        Arrays.sort(arr);
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+        for(int i = 0; i < T; i++) {
+            int d = (int) arr[i];
+            int ansA = d - 100000;
+            int ansB = (int) (Math.round((arr[i] - d) * 1000000) - 100000);
+            sb.append(ansA + " " + ansB + "\n");
         }
 
         System.out.println(sb.toString());
 
     }
+
 }
