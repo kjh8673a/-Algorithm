9037448c4322c553666d213c88277fdc21a2f7e1
Jihoon Gweon <kjh8673a@naver.com>
2023-03-31 22:28:46 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 236 ms, Memory: 77564 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        int ans = 0;
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    ans = Math.max(ans, dp[i + 1][j + 1]);
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
