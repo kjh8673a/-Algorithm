c85294d3a051ff87a1bb79d78abae4ac37fe24ea
Jihoon Gweon <kjh8673a@naver.com>
2023-04-08 20:19:13 +0900

    [Bronze V] Title: A+B - 9, Time: 244 ms, Memory: 17384 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+
+        System.out.println(a.add(b));
+    }
+
+}
