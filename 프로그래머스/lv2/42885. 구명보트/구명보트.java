73e0e5413e5b1a6df4bc508002829f150f11e931
Jihoon Gweon <kjh8673a@naver.com>
2023-05-06 10:41:07 +0900

    [level 2] Title: 구명보트, Time: 8.26 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+                       
+        int answer = 0;
+        while(left <= right) {
+            if(left == right) {
+                answer++;
+                break;
+            }
+            if(people[left] + people[right] <= limit) {
+                answer++;
+                left++;
+                right--;
+            }else {
+                answer++;
+                right--;
+            }
+         }                                                  
+        
+        return answer;
+    }
+}
