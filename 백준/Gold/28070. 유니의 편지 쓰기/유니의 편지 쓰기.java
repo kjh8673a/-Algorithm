510cab59bcc55a22834b99e27e397061e0f3dbd6
Jihoon Gweon <kjh8673a@naver.com>
2023-05-26 13:30:39 +0900

    [Gold V] Title: 유니의 편지 쓰기, Time: 632 ms, Memory: 120852 KB -BaekjoonHub

--- 백준/Gold/28070. 유니의 편지 쓰기/유니의 편지 쓰기.java
+++ 백준/Gold/28070. 유니의 편지 쓰기/유니의 편지 쓰기.java
@@ -5,50 +5,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[10000][13];
+        int[] dp = new int[120002];
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String a = st.nextToken();
             int start_year = Integer.parseInt(a.split("-")[0]);
             int start_month = Integer.parseInt(a.split("-")[1]);
-            dp[start_year][start_month]++;
+            dp[start_year * 12 + start_month]++;
 
             String b = st.nextToken();
             int end_year = Integer.parseInt(b.split("-")[0]);
             int end_month = Integer.parseInt(b.split("-")[1]);
-            if (end_year == 9999 && end_month == 12) {
-                continue;
-            }
-            if (end_month == 12) {
-                dp[end_year + 1][1]--;
-            } else {
-                dp[end_year][end_month + 1]--;
-            }
+            dp[end_year * 12 + end_month + 1]--;
 
         }
 
-        for (int i = 2000; i < 10000; i++) {
-            for (int j = 1; j < 13; j++) {
-                if (j == 1) {
-                    dp[i][j] += dp[i - 1][12];
-                } else {
-                    dp[i][j] += dp[i][j - 1];
-                }
-            }
+        for (int i = 24000; i < 120001; i++) {
+            dp[i] += dp[i - 1];
         }
 
         int max = 0;
         int year = 0;
         int month = 0;
-        for (int i = 2000; i < 10000; i++) {
-            for (int j = 1; j < 13; j++) {
-                if (dp[i][j] > max) {
-                    max = dp[i][j];
-                    year = i;
-                    month = j;
-                }
+        for (int i = 24000; i < 120001; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+                year = i / 12;
+                month = i % 12;
             }
+        }
+
+        if (month == 0) {
+            year--;
+            month = 12;
         }
 
         if (month < 10) {
