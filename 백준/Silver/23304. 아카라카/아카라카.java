e275af714f2e6725173f41c013e7299f6680f77a
Jihoon Gweon <kjh8673a@naver.com>
2023-06-01 21:31:08 +0900

    [Silver II] Title: 아카라카, Time: 408 ms, Memory: 153740 KB -BaekjoonHub

---
+++ 백준/Silver/23304. 아카라카/아카라카.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        if (check(word)) {
+            System.out.println("AKARAKA");
+        } else {
+            System.out.println("IPSELENTI");
+        }
+
+    }
+
+    private static boolean check(String word) {
+        if (word.length() == 1) {
+            return true;
+        }
+
+        int len = word.length() / 2;
+        String front = word.substring(0, len);
+        String back = word.substring(word.length() - len, word.length());
+
+        if (!front.equals(back)) {
+            return false;
+        }
+
+        if (check(front) && check(back)) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
