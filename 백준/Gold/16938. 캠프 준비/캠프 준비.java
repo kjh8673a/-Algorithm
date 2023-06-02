53f4b29c6b22d568d18bee27985b5e1b318e7c9d
Jihoon Gweon <kjh8673a@naver.com>
2023-06-02 14:45:30 +0900

    [Gold V] Title: 캠프 준비, Time: 148 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -1,73 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static int N, L, R, X;
-    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         int ans = 0;
-        boolean[] visit;
         for (int i = 1; i < (1 << N); i++) {
-            visit = new boolean[N];
             int len = 0;
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            int sum = 0;
+
             for (int j = 0; j < N; j++) {
                 if ((i & (1 << j)) != 0) {
-                    visit[j] = true;
                     len++;
+                    max = Math.max(arr[j], max);
+                    min = Math.min(arr[j], min);
+                    sum += arr[j];
                 }
             }
-            if (len == 1) {
+
+            if (len == 1) { // 문제는 두 문제 이상이어야 한다
                 continue;
             }
-            if (check(visit)) {
-                ans++;
+            if (sum < L || sum > R) { // 문제 난이도의 합은 L보다 크거나 같고, R보다 작거나 같아야 한다
+                continue;
             }
+            if (max - min < X) { // 가장 어려운 문제와 가장 쉬운 문제의 난이도 차이는 X보다 크거나 같아야 한다
+                continue;
+            }
+
+            ans++;
+
         }
 
         System.out.println(ans);
 
-    }
-
-    private static boolean check(boolean[] visit) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                continue;
-            }
-
-            max = Math.max(arr[i], max);
-            min = Math.min(arr[i], min);
-            sum += arr[i];
-        }
-
-        if (sum < L || sum > R) {
-            return false;
-        }
-
-        if (max - min < X) {
-            return false;
-        }
-
-        return true;
     }
 
 }
