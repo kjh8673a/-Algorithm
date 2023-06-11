6febba99ad874a94094ab523df6fd9754c763b00
Jihoon Gweon <kjh8673a@naver.com>
2023-06-11 20:51:54 +0900

    [Silver III] Title: 에너지 드링크, Time: 456 ms, Memory: 31208 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        double[] arr = new double[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < N - 1; i++) {
+            arr[N - 1] += arr[i] / 2;
+        }
+
+        System.out.println(arr[N - 1]);
+    }
+
+}
