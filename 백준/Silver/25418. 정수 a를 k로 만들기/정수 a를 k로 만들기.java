69e401f3ee7422e3a2b316bb242efb306072660e
Jihoon Gweon <kjh8673a@naver.com>
2023-06-27 21:19:21 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 136 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -1,29 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
+        int[] dp = new int[K + 1];
+        for (int i = A + 1; i <= K; i++) {
+            if (i % 2 == 0 && i / 2 >= A) {
+                dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
+            } else {
+                dp[i] = dp[i - 1] + 1;
+            }
+        }
 
-		int[] dp = new int[k + 1];
-		dp[a] = 0;
-		for (int i = a + 1; i < k + 1; i++) {
-			if (i % 2 == 0 && i / 2 >= a) {
-				dp[i] = Math.min(dp[i - 1], dp[i / 2]) + 1;
-			} else {
-				dp[i] = dp[i - 1] + 1;
-			}
-		}
-
-		System.out.println(dp[k]);
-
-	}
+        System.out.println(dp[K]);
+    }
 
 }
