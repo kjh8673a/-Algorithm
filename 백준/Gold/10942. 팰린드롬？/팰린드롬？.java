c2822c7c3af1e23c375e39d3e31f9bae1f69e045
Jihoon Gweon <kjh8673a@naver.com>
2023-07-09 12:59:29 +0900

    [Gold IV] Title: 팰린드롬?, Time: 828 ms, Memory: 220064 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,39 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] arr;
+    static boolean[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
+        arr = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i < N + 1; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        boolean[][] dp = new boolean[N + 1][N + 1];
-        for (int i = 1; i < N + 1; i++) {
-            dp[i][i] = true;
-        }
-        for (int i = 1; i < N; i++) {
-            if (arr[i] == arr[i + 1]) {
-                dp[i][i + 1] = true;
-            }
-        }
-        for (int i = 2; i < N; i++) {
-            for (int j = 1; i + j < N + 1; j++) {
-                if (arr[j] == arr[i + j] && dp[j + 1][i + j - 1]) {
-                    dp[j][i + j] = true;
-                }
-            }
-        }
-
         int M = Integer.parseInt(br.readLine());
+        dp = new boolean[N + 1][N + 1];
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
+            pal(S, E);
             if (dp[S][E]) {
                 sb.append(1).append("\n");
             } else {
@@ -45,4 +32,23 @@ public class Main {
         System.out.println(sb);
     }
 
+    private static boolean pal(int s, int e) {
+        if (dp[s][e]) {
+            return true;
+        }
+
+        if (s == e) {
+            return  dp[s][e] = true;
+        } else {
+            if (arr[s] == arr[e]) {
+                if (s + 1 == e || pal(s + 1, e - 1)) {
+                    return dp[s][e] = true;
+                } else {
+                    return dp[s][e] = false;
+                }
+            } else {
+                return dp[s][e] = false;
+            }
+        }
+    }
 }
