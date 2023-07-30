8184a671500dbc7b20ce27eb6771984304e2250b
Jihoon Gweon <kjh8673a@naver.com>
2023-07-30 11:01:32 +0900

    [Gold V] Title: 트리와 쿼리, Time: 812 ms, Memory: 77800 KB -BaekjoonHub

---
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> tree;
+    static int[] dp;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        tree = new ArrayList<>();
+        for (int i = 1; i <= N + 1; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            tree.get(u).add(v);
+            tree.get(v).add(u);
+        }
+
+        dp = new int[N + 1];
+        visit = new boolean[N + 1];
+        visit[R] = true;
+        count(R);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            int u = Integer.parseInt(br.readLine());
+            sb.append(dp[u]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int count(int n) {
+        int total = 1;
+        for (int next : tree.get(n)) {
+            if (!visit[next]) {
+                visit[next] = true;
+                total += count(next);
+            }
+        }
+
+        return dp[n] = total;
+    }
+
+}
