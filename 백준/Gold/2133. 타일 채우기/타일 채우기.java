b89f591145b61d3fb50ce92766395a25729794f5
Jihoon Gweon <kjh8673a@naver.com>
2023-08-08 09:32:14 +0900

    [Gold IV] Title: 타일 채우기, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+
+        for (int i = 2; i < N + 1; i += 2) {
+            dp[i] = dp[i - 2] * 3;
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
