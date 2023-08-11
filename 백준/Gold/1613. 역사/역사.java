ba6a1a03ada1cac07b01f201c54b49bb5115fcf1
Jihoon Gweon <kjh8673a@naver.com>
2023-08-11 11:16:48 +0900

    [Gold III] Title: 역사, Time: 560 ms, Memory: 38024 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean[][] table = new boolean[N + 1][N + 1];
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            table[a][b] = true;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (table[i][k] && table[k][j]) {
+                        table[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int S = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (table[a][b]) {
+                sb.append(-1).append("\n");
+            } else if (table[b][a]) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
