7cb1eb3bd524eb500107e87fd2e6e8c7c4209d69
Jihoon Gweon <kjh8673a@naver.com>
2023-08-13 11:42:15 +0900

    [Gold III] Title: 백양로 브레이크, Time: 440 ms, Memory: 31772 KB -BaekjoonHub

---
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (i != j) {
+                    dist[i][j] = 100000000;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            dist[u][v] = 0;
+            dist[v][u] = b == 0 ? 1 : 0;
+        }
+
+        for (int k = 1; k < n + 1; k++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int k = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            sb.append(dist[s][e]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+}
