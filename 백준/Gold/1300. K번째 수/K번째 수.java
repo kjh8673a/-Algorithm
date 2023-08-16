e39be71b51a64345c01d07362fdcfebe8982aa75
Jihoon Gweon <kjh8673a@naver.com>
2023-08-16 11:24:37 +0900

    [Gold II] Title: K번째 수, Time: 164 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,31 +1,37 @@
 import java.io.*;
 
 public class Main {
+    static int N, k;
+    static long ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int k = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
 
-        long left = 1;
-        long right = k;
+        ans = binarySearch(1, k);
 
-        while (left < right) {
-            long mid = (left + right) / 2;
+        System.out.println(ans);
+    }
 
-            long cnt = 0;
-            for (int i = 1; i <= N; i++) {
-                cnt += Math.min(mid / i, N);
-            }
-
-            if (cnt >= k) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
+    private static long binarySearch(long left, long right) {
+        if (left >= right) {
+            return left;
         }
 
-        System.out.println(left);
+        long mid = (left + right) / 2;
+
+        long cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            cnt += Math.min(mid / i, N);
+        }
+
+        if (cnt >= k) {
+            return binarySearch(left, mid);
+        } else {
+            return binarySearch(mid + 1, right);
+        }
+
     }
 
 }
