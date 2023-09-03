62055e62ba4aaafb88db623d9396e1468b4c69a5
Jihoon Gweon <kjh8673a@naver.com>
2023-09-03 11:45:55 +0900

    [Gold IV] Title: 운동, Time: 696 ms, Memory: 67480 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static final int INF = 1_000_000_000;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[v + 1][v + 1];
+        for (int i = 1; i < v + 1; i++) {
+            for (int j = 1; j < v + 1; j++) {
+                dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = c;
+        }
+
+        for (int k = 1; k < v + 1; k++) {
+            for (int i = 1; i < v + 1; i++) {
+                for (int j = 1; j < v + 1; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        int ans = INF;
+        for (int i = 1; i < v + 1; i++) {
+            ans = Math.min(ans, dist[i][i]);
+        }
+
+        System.out.println(ans == INF ? -1 : ans);
+    }
+
+}
