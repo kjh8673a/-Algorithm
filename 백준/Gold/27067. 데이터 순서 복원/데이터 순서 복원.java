9c3785b7ea8b3595329dee7bc5900cd3c85ccfdf
Jihoon Gweon <kjh8673a@naver.com>
2023-12-28 10:45:08 +0900

    [Gold III] Title: 데이터 순서 복원, Time: 316 ms, Memory: 20088 KB -BaekjoonHub

---
+++ 백준/Gold/27067. 데이터 순서 복원/데이터 순서 복원.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[3][N];
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[] answer = new int[N];
+        boolean[] visited = new boolean[N + 1];
+        int idx = N - 1;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < 3; j++) {
+                int num = arr[j][i];
+                if (visited[num]) {
+                    continue;
+                }
+                if (!queue.contains(num)) {
+                    queue.add(num);
+                } else {
+                    answer[idx--] = num;
+                    visited[num] = true;
+                }
+            }
+            while (!queue.isEmpty()) {
+                int num = queue.poll();
+                if (!visited[num]) {
+                    answer[idx--] = num;
+                    visited[num] = true;
+                }
+            }
+        }
+
+        Arrays.stream(answer).forEach(num -> System.out.print(num + " "));
+    }
+
+}
