35b76bc667d88d35154182f8de7b6f20b00c7ae5
Jihoon Gweon <kjh8673a@naver.com>
2024-01-01 21:21:29 +0900

    [Bronze V] Title: Bottle Return, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int answer = 0;
+        for (int i = 0; i < 6; i++) {
+            answer += Integer.parseInt(st.nextToken()) * 5;
+        }
+
+        System.out.println(answer);
+    }
+
+}
