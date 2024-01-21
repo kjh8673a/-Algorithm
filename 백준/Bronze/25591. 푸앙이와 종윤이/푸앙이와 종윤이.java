1a4890c9148d502655358a6ddb8ecb0c1eb2e4fe
Jihoon Gweon <kjh8673a@naver.com>
2024-01-21 20:49:01 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 204 ms, Memory: 17048 KB -BaekjoonHub

---
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int a = 100 - x;
+        int b = 100 - y;
+        int c = 100 - (a + b);
+        int d = a * b;
+        int q = d / 100;
+        int r = d % 100;
+
+        System.out.println(a + " " + b + " " + c + " " + d + " " + q + " " + r);
+        System.out.println((c + q) + " " + r);
+    }
+
+}
