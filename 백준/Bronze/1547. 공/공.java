6a187bf1861af6a140e297c7ba7f2d1143c57b63
Jihoon Gweon <kjh8673a@naver.com>
2024-01-23 03:15:08 +0900

    [Bronze III] Title: 공, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M = Integer.parseInt(br.readLine());
+
+        int ball = 1;
+        StringTokenizer st;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if (x == ball) {
+                ball = y;
+            } else if (y == ball) {
+                ball = x;
+            }
+        }
+
+        System.out.println(ball);
+    }
+
+}
