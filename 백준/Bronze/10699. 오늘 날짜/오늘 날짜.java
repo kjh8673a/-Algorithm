a48711deab8f9ac39b99d81e46bd4dc0a499ae85
Jihoon Gweon <kjh8673a@naver.com>
2024-02-24 16:07:25 +0900

    [Bronze V] Title: 오늘 날짜, Time: 112 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.time.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(LocalDate.now(ZoneId.of("UTC")));
+    }
+
+}
