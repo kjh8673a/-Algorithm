11ee923cba58bc244332727fe668e2e324e074c7
Jihoon Gweon <kjh8673a@naver.com>
2024-03-27 19:26:56 +0900

    [Silver II] Title: 격자상의 경로, Time: 132 ms, Memory: 14232 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        if (k > 0) {
+            int nr = k / m;
+            int nc = k % m - 1;
+            System.out.println(doDp(nr + 1, nc + 1) * doDp(n - nr, m - nc));
+
+        } else {
+            System.out.println(doDp(n, m));
+        }
+    }
+
+    private static int doDp(int r, int c) {
+        int[][] dp = new int[r + 1][c + 1];
+        dp[0][1] = 1;
+        for (int i = 1; i < r + 1; i++) {
+            for (int j = 1; j < c + 1; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[r][c];
+    }
+
+}
