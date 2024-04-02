dae698c7dc41662812ef8024d72a9c3062e40e2d
Jihoon Gweon <kjh8673a@naver.com>
2024-04-02 16:17:18 +0900

    [Silver V] Title: 원, 탁!, Time: 468 ms, Memory: 91172 KB -BaekjoonHub

---
+++ 백준/Silver/28136. 원， 탁！/원， 탁！.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[(i + n - 1) % n] >= arr[i]) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
