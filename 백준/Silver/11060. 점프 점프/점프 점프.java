c7891b883aba3bd97cdf0c84e3f2c0339c8a5a7d
Jihoon Gweon <kjh8673a@naver.com>
2024-05-17 16:25:37 +0900

    [Silver II] Title: 점프 점프, Time: 136 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0 || dp[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+            for (int j = 1; j <= arr[i]; j++) {
+                if (i + j < n) {
+                    dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[n - 1] == Integer.MAX_VALUE ? -1 : dp[n - 1]);
+    }
+
+}
