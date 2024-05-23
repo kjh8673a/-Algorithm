dd38c9469c970ba0656ddc904f2b268aa2bef4f9
Jihoon Gweon <kjh8673a@naver.com>
2024-05-23 17:23:50 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 144 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] card = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < n + 1; i++) {
+            card[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + card[j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
