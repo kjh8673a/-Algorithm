4dea25826a993d57431130d6cba9b40334e04f5b
Jihoon Gweon <kjh8673a@naver.com>
2024-06-26 10:02:49 +0900

    [Bronze V] Title: 등록, Time: 120 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(1217);
+        System.out.println("kjh8673a");
+    }
+
+}
