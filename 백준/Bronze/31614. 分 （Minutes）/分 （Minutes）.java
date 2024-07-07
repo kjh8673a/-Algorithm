43c78f232cd7ffa3dbef104af81e44e91bb4b5a9
Jihoon Gweon <kjh8673a@naver.com>
2024-07-07 20:03:26 +0900

    [Bronze V] Title: 分 (Minutes), Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        System.out.println(n * 60 + m);
+    }
+
+}
