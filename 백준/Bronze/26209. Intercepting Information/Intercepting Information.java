879a022995941a59898802d4522c09b0854567dc
Jihoon Gweon <kjh8673a@naver.com>
2024-07-16 14:32:02 +0900

    [Bronze V] Title: Intercepting Information, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/26209. Intercepting Information/Intercepting Information.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        if (line.contains("9")) {
+            System.out.println("F");
+        } else {
+            System.out.println("S");
+        }
+    }
+
+}
