94d0de672922f43289d900ac6b50badfad519435
Jihoon Gweon <kjh8673a@naver.com>
2024-07-18 10:28:34 +0900

    [Gold V] Title: 수강 과목, Time: 208 ms, Memory: 52156 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -2,38 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static class Node {
-        int value;
-        int cost;
-
-        public Node(int value, int cost) {
-            this.value = value;
-            this.cost = cost;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        Node[] list = new Node[k + 1];
+        int[][] subject = new int[k + 1][2];
+
         for (int i = 1; i < k + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int v = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            list[i] = new Node(v, c);
+            subject[i][0] = Integer.parseInt(st.nextToken());
+            subject[i][1] = Integer.parseInt(st.nextToken());
         }
 
         int[][] dp = new int[k + 1][n + 1];
         for (int i = 1; i < k + 1; i++) {
-            int value = list[i].value;
-            int cost = list[i].cost;
-            for (int j = 0; j < n + 1; j++) {
-                if (j >= cost) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + value);
+            int importance = subject[i][0];
+            int time = subject[i][1];
+
+            for (int j = 1; j < n + 1; j++) {
+                if (time <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - time] + importance);
                 } else {
                     dp[i][j] = dp[i - 1][j];
                 }
