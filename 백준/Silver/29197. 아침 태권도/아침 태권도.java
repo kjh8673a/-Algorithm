13ce3eb6a64b7c98c0c252398617deee93fe1489
Jihoon Gweon <kjh8673a@naver.com>
2024-08-10 20:29:04 +0900

    [Silver II] Title: 아침 태권도, Time: 600 ms, Memory: 101688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/29197. 아침 태권도/아침 태권도.java
+++ 백준/Silver/29197. 아침 태권도/아침 태권도.java
@@ -6,31 +6,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        HashSet<String> set = new HashSet<>();
+        HashSet<Double> quad1 = new HashSet<>();
+        HashSet<Double> quad2 = new HashSet<>();
+        HashSet<Double> quad3 = new HashSet<>();
+        HashSet<Double> quad4 = new HashSet<>();
+        HashSet<Integer> axis = new HashSet<>();
+
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            double a = Double.parseDouble(st.nextToken());
+            double b = Double.parseDouble(st.nextToken());
 
-            if (a != 0 && b != 0) {
-                int gcd = getGCD(a, b);
-                set.add(a / gcd + "::" + b / gcd);
-            } else if (a == 0) {
-                set.add(0 + "::" + b / Math.abs(b));
-            } else {
-                set.add(a / Math.abs(a) + "::" + 0);
+            if (a > 0 && b > 0) {
+                quad1.add(a / b);
+            } else if (a < 0 && b > 0) {
+                quad2.add(a / b);
+            } else if (a < 0 && b < 0) {
+                quad3.add(a / b);
+            } else if (a > 0 && b < 0) {
+                quad4.add(a / b);
+            } else if (a == 0 && b > 0) {
+                axis.add(1);
+            } else if (a == 0 && b < 0) {
+                axis.add(2);
+            } else if (a > 0 && b == 0) {
+                axis.add(3);
+            } else if (a < 0 && b == 0) {
+                axis.add(4);
             }
         }
 
-        System.out.println(set.size());
-    }
+        int answer = quad1.size() + quad2.size() + quad3.size() + quad4.size() + axis.size();
 
-    private static int getGCD(int num1, int num2) {
-        if (num1 % num2 == 0) {
-            return num2;
-        }
-        return getGCD(num2, num1 % num2);
+        System.out.println(answer);
     }
 
 }
