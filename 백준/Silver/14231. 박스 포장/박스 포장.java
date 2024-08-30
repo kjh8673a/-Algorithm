24100998dd0663493a3a226b42030438a71cd3dd
Jihoon Gweon <kjh8673a@naver.com>
2024-08-30 12:24:42 +0900

    [Silver II] Title: 박스 포장, Time: 168 ms, Memory: 14640 KB -BaekjoonHub

---
+++ 백준/Silver/14231. 박스 포장/박스 포장.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] box = new int[n];
+        for (int i = 0; i < n; i++) {
+            box[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (box[i] > box[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+    }
+
+}
