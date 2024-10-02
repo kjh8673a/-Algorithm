7e25fd4c5ebe2e923ca53c14feae3e040ac3cb13
Jihoon Gweon <kjh8673a@naver.com>
2024-10-02 14:31:06 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 208 ms, Memory: 20352 KB -BaekjoonHub

---
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int max = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += num;
+            max = Math.max(num, max);
+        }
+
+        System.out.println(sum - max);
+    }
+
+}
