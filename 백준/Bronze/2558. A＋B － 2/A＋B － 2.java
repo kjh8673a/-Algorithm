b71d5b72753255deffd21d8b9ebfd3e0a414b9e2
Jihoon Gweon <kjh8673a@naver.com>
2024-12-23 14:47:32 +0900

    [Bronze V] Title: A+B - 2, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);		
-		System.out.println(sc.nextInt() + sc.nextInt());
-        sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A + B);
+    }
 }
