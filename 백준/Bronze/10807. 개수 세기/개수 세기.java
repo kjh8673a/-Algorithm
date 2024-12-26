75e07a2b559ee257f4395755e30ac6a3fa5581f8
Jihoon Gweon <kjh8673a@naver.com>
2024-12-26 21:48:33 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,22 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        int v = Integer.parseInt(br.readLine());
-        if (map.containsKey(v)) {
-            System.out.println(map.get(v));
-        } else {
-            System.out.println(0);
-        }
-    }
 
+        int[] count = new int[201];
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken()) + 100;
+            count[a]++;
+        }
+        int v = Integer.parseInt(br.readLine()) + 100;
+
+        System.out.println(count[v]);
+    }
 }
