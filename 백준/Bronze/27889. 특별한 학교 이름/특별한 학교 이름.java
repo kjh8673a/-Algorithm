cb38dd457e0609b84d24382e9d628243836bc2fd
Jihoon Gweon <kjh8673a@naver.com>
2024-12-27 14:41:26 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 104 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -1,19 +1,20 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         Map<String, String> map = new HashMap<>() {
             {
                 put("NLCS", "North London Collegiate School");
                 put("BHA", "Branksome Hall Asia");
                 put("KIS", "Korea International School");
                 put("SJA", "St. Johnsbury Academy");
-            };
+            }
         };
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(map.get(br.readLine()));
-    }
+        String str = br.readLine();
 
+        System.out.println(map.get(str));
+    }
 }
