0f2f44073ceaa2b7a3050b1a2f34696b77619f59
Jihoon Gweon <kjh8673a@naver.com>
2024-12-27 14:46:53 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/26082. WARBOY/WARBOY.java
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -1,15 +1,15 @@
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException, NumberFormatException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-        System.out.println((B / A * 3) * C);
-    }
 
+        int WARBOY = 3 * B / A;
+        System.out.println(WARBOY * C);
+    }
 }
