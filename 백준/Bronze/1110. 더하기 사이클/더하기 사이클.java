8f3bfd0daadbed71744f30dcf3ff842dd4f26ed8
Jihoon Gweon <kjh8673a@naver.com>
2024-12-31 18:54:45 +0900

    [Bronze I] Title: 더하기 사이클, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,22 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
 
-        int i = 0;
-        int tmp = n;
-        while (i >= 0) {
-            n = (n%10)*10 + (n/10 +n%10)%10;
-            i++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int start = n;
 
-            if(tmp == n) {
+        int cycle = 0;
+        while (true) {
+            cycle++;
+            int a = n / 10;
+            int b = n % 10;
+            n = b * 10 + (a + b) % 10;
+
+            if (start == n) {
                 break;
             }
         }
-        System.out.println(i);
 
+        System.out.println(cycle);
     }
 }
