3fc52e8d07e105a45de4520c1c8d7e5988acc08b
Jihoon Gweon <kjh8673a@naver.com>
2025-01-03 09:12:20 +0900

    [Bronze IV] Title: 전자레인지, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/14470. 전자레인지/전자레인지.java
+++ 백준/Bronze/14470. 전자레인지/전자레인지.java
@@ -1,27 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		int D = sc.nextInt();
-		int E = sc.nextInt();
-		sc.close();
-		
-		int ans = 0;
-		if(A < 0) {
-			if(B < 0) {
-				ans = (B-A)*C;
-			}else {
-				ans = (0-A)*C + D + (B-0)*E;
-			}
-		}else {
-			ans = (B-A)*E;
-		}
-		
-		System.out.println(ans);
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int r = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+        if (a < 0) {
+            answer += (0 - a) * c;
+            answer += d;
+        }
+        answer += Math.min(b - a, b - 0) * r;
+
+        System.out.println(answer);
+    }
 }
