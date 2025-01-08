2f41418b6b640a22c11fa9d76065451083541bd0
Jihoon Gweon <kjh8673a@naver.com>
2025-01-08 23:37:33 +0900

    [Bronze V] Title: Hello World, Time: 88 ms, Memory: 13872 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,8 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
     }
 }
