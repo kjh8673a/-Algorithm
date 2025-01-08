3027bd6b185080f50d765902bd1f2a607b31247e
Jihoon Gweon <kjh8673a@naver.com>
2025-01-08 23:36:36 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,20 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int sum = 0;
-		for (int i = 0; i < 5; i++) {
-			sum += Math.pow(Integer.parseInt(st.nextToken()), 2);
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sumSquare = 0;
+        for (int i = 0; i < 5; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            sumSquare += a * a;
+        }
 
-		System.out.println(sum % 10);
-
-	}
-
+        System.out.println(sumSquare % 10);
+    }
 }
