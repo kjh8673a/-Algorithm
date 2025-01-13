e252d732ddb63541035682cc17601ccd7f9f4e89
Jihoon Gweon <kjh8673a@naver.com>
2025-01-13 15:55:56 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,27 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		
-		int[] arr = new int[10];
-		
-		int num = A * B * C;
-		
-		while(num > 0) {
-			arr[num % 10]++;
-			num /= 10;
-		}
-		for(int i = 0; i < 10; i++) {
-			System.out.println(arr[i]);
-		}
-		
-		sc.close();
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        int num = a * b * c;
+        int[] count = new int[10];
+        while (num > 0) {
+            count[num % 10]++;
+            num /= 10;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(count[i]);
+        }
+    }
+
 }
