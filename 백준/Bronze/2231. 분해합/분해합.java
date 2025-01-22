3071871310a227a2429d4e49baec5df65a7c3971
Jihoon Gweon <kjh8673a@naver.com>
2025-01-22 13:13:34 +0900

    [Bronze II] Title: 분해합, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,30 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         int answer = 0;
-
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            int num = i;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
-            }
-
-            if (sum + i == n) {
+        for (int i = 1; i < n; i++) {
+            if (makeSum(i) == n) {
                 answer = i;
                 break;
             }
         }
 
         System.out.println(answer);
+    }
+
+    private static int makeSum(int num) {
+        int sum = num;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
     }
 
 }
