849b13d9731f380886fa88d31ef0a1eb8ff725d9
Jihoon Gweon <kjh8673a@naver.com>
2025-01-29 21:26:08 +0900

    [Bronze II] Title: 대표값, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2592. 대표값/대표값.java
+++ 백준/Bronze/2592. 대표값/대표값.java
@@ -2,43 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Numbers {
-        List<Long> list = new ArrayList<>();
-
-        public void insert(long num) {
-            list.add(num);
-        }
-
-        public long getAverage() {
-            return (long) list.stream().mapToLong(i -> i).average().orElse(0);
-        }
-
-        public long getMode() {
-            long[] distinct = list.stream().distinct().mapToLong(i -> i).toArray();
-
-            long max = 0;
-            long result = 0;
-            for (long num : distinct) {
-                long count = list.stream().filter(i -> i == num).count();
-                if (count > max) {
-                    max = count;
-                    result = num;
-                }
-            }
-
-            return result;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Numbers numbers = new Numbers();
+
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < 10; i++) {
-            numbers.insert(Long.parseLong(br.readLine()));
+            int a = Integer.parseInt(br.readLine());
+            map.put(a, map.getOrDefault(a, 0) + 1);
         }
 
-        System.out.println(numbers.getAverage());
-        System.out.println(numbers.getMode());
+        int sum = 0;
+        int mode = 0;
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > count) {
+                mode = entry.getKey();
+                count = entry.getValue();
+            }
+
+            sum += entry.getKey() * entry.getValue();
+        }
+
+        System.out.println(sum / 10);
+        System.out.println(mode);
     }
 
 }
