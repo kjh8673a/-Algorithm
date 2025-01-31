2e3c431574763b79eba9eb08ee3b5c8596e57c9f
Jihoon Gweon <kjh8673a@naver.com>
2025-01-31 16:44:36 +0900

    [Silver V] Title: 숫자 카드, Time: 876 ms, Memory: 135664 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,44 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Set<Integer> set = new HashSet<>();
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int j = 0; j < n; j++) {
-            arr[j] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(arr);
 
+        StringBuilder sb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        for (int j = 0; j < m; j++) {
-            int check = Integer.parseInt(st.nextToken());
-            int ans = binarySearch(arr, 0, arr.length - 1, check);
-            sb.append(String.valueOf(ans) + " ");
+        for (int i = 0; i < m; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            sb.append(set.contains(a) ? 1 : 0).append(" ");
         }
 
         System.out.println(sb.toString());
-
     }
 
-    public static int binarySearch(int[] a, int start, int end, int key) {
-        if (start > end) return 0;
-
-        int mid = (start + end) / 2;
-        if (a[mid] > key) return binarySearch(a, start, mid - 1, key);
-        else if (a[mid] < key) return binarySearch(a, mid + 1, end, key);
-        else return 1;
-    }
 }
