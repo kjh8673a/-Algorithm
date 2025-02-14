edd66358fdbcb578a637df8e0b418bda8e5c408e
Jihoon Gweon <kjh8673a@naver.com>
2025-02-14 09:55:12 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,21 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
 
-		int x = Integer.parseInt(br.readLine());
-
-		int cnt = 0;
-		for(char ch : Integer.toBinaryString(x).toCharArray()) {
-			if(ch == '1') {
-				cnt++;
-			}
-		}
-
-		System.out.println(cnt);
-	}
+        System.out.println(Integer.bitCount(x));
+    }
 
 }
