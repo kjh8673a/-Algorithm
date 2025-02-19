36abfa0d0b0e6505d525264a417bbb162a98208c
Jihoon Gweon <kjh8673a@naver.com>
2025-02-19 12:08:26 +0900

    [Silver II] Title: 과일 서리, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/17213. 과일 서리/과일 서리.java
+++ 백준/Silver/17213. 과일 서리/과일 서리.java
@@ -1,23 +1,26 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][m + 1];
 
-        int[][] dp = new int[N + 1][M + 1];
-        for (int i = 1; i < M + 1; i++) {
+        for (int i = 1; i <= m; i++) {
             dp[1][i] = 1;
         }
-
-        for (int i = 2; i < N + 1; i++) {
-            for (int j = i; j < M + 1; j++) {
+        for (int i = 1; i <= n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= m; j++) {
                 dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
             }
         }
-        
-        System.out.println(dp[N][M]);
+
+        System.out.println(dp[n][m]);
     }
 
 }
