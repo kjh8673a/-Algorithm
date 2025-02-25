00e9eda870c426ef59830666ca71594950b5a8b0
Jihoon Gweon <kjh8673a@naver.com>
2025-02-25 11:19:50 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14268 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,27 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
+import java.io.*;
 
 public class Main {
-	static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        String s = br.readLine();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
+        long answer = 0;
+        for (int i = 0; i < l; i++) {
+            long a = s.charAt(i) - 'a' + 1;
+            answer = (long) (answer + a * Math.pow(31, i)) % 1234567891;
+        }
 
-		String str = br.readLine();
-
-		BigInteger num = new BigInteger("0");
-		for(int i = 0; i < n; i++) {
-			int ch = str.charAt(i) - 'a' + 1;
-
-			num = num.add(BigInteger.valueOf(ch).multiply(BigInteger.valueOf(31).pow(i)));
-		}
-
-		System.out.println(num.remainder(BigInteger.valueOf(1234567891)));
-
-	}
+        System.out.println(answer);
+    }
 
 }
