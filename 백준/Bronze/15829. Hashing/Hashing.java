3f297802831638afd2760c9700d665495a7964db
Jihoon Gweon <kjh8673a@naver.com>
2025-02-25 11:23:59 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14400 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,18 +1,22 @@
 import java.io.*;
 
 public class Main {
+    static final int MOD = 1234567891;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int l = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
         long answer = 0;
+        long pow = 1;
         for (int i = 0; i < l; i++) {
             long a = s.charAt(i) - 'a' + 1;
-            answer = (long) (answer + a * Math.pow(31, i)) % 1234567891;
+            answer += a * pow % MOD;
+            pow = (pow * 31) % MOD;
         }
 
-        System.out.println(answer);
+        System.out.println(answer % MOD);
     }
 
 }
