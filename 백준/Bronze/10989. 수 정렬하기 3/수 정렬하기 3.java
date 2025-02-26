88fd4375ca16bf56a8ef6421ee886f8948e6859f
Jihoon Gweon <kjh8673a@naver.com>
2025-02-26 15:17:29 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1572 ms, Memory: 339328 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,42 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
+        int[] arr = new int[10001];
         int n = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[n];
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            max = Math.max(max, arr[i]);
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
         }
 
-        int[] counting = new int[max + 1];
-        for(int i = 0; i < arr.length; i++) {
-            counting[arr[i]]++;
-        }
-
-        for(int i = 1; i < counting.length; i++) {
-            counting[i] += counting[i-1];
-        }
-
-        int[] result = new int[n];
-        for(int i = arr.length-1; i >= 0; i--) {
-            counting[arr[i]]--;
-            result[counting[arr[i]]] = arr[i];
-        }
-
-        for(int i = 0; i < result.length; i++) {
-            sb.append(result[i] + "\n");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] > 0) {
+                sb.append(i).append("\n");
+                arr[i]--;
+            }
         }
 
         System.out.println(sb.toString());
-
     }
 
 }
