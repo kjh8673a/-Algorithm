616d9c542231fd6aabf71c454766da3008a1ed82
Jihoon Gweon <kjh8673a@naver.com>
2025-02-26 13:09:40 +0900

    [Bronze III] Title: 직각삼각형, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -8,11 +8,11 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while (true) {
             int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+            Arrays.sort(arr);
+            if (arr[0] + arr[1] + arr[2] == 0) {
                 break;
             }
 
-            Arrays.sort(arr);
             if (arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2]) {
                 sb.append("right").append("\n");
             } else {
