a105bec5b07de675ed521d16a96fc4e49eaf53a6
Jihoon Gweon <kjh8673a@naver.com>
2025-02-26 13:35:53 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,62 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Integer> mapA = new HashMap<>();
-        while(a > 1) {
-            for(int i = 2; i <= a; i++) {
-                if(a % i == 0) {
-                    mapA.put(i, mapA.getOrDefault(i, 0) + 1);
-                    a /= i;
-                    break;
-                }
-            }
+        int gcd = getGcd(Math.max(a, b), Math.min(a, b));
+        int lcm = a * b / gcd;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    private static int getGcd(int a, int b) {
+        if (b == 0) {
+            return a;
         }
-
-        Map<Integer, Integer> mapB = new HashMap<>();
-        while(b > 1) {
-            for(int i = 2; i <= b; i++) {
-                if(b % i == 0) {
-                    mapB.put(i, mapB.getOrDefault(i, 0) + 1);
-                    b /= i;
-                    break;
-                }
-            }
-        }
-
-        int small = 1;
-        int big = 1;
-
-        for(int nKey : mapA.keySet()) {
-            if(mapB.containsKey(nKey)) {
-                small *= Math.pow(nKey, Math.min(mapA.get(nKey), mapB.get(nKey)));
-                big *= Math.pow(nKey, Math.max(mapA.get(nKey), mapB.get(nKey)));
-            }else {
-                big *= Math.pow(nKey, mapA.get(nKey));
-            }
-        }
-
-        for(int nKey : mapB.keySet()) {
-            if(!mapA.containsKey(nKey)) {
-                big *= Math.pow(nKey, mapB.get(nKey));
-            }
-        }
-
-        System.out.println(small);
-        System.out.println(big);
-
+        return getGcd(b, a % b);
     }
 
 }
