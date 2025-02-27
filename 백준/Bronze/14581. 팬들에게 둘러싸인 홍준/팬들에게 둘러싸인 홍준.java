718f8a8e40b19f695f8b52592a7a3bf77fdebd15
Jihoon Gweon <kjh8673a@naver.com>
2025-02-28 08:33:51 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 112 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -1,13 +1,12 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String name = br.readLine();
+        String id = br.readLine();
 
         System.out.println(":fan::fan::fan:");
-        System.out.println(":fan::" + name + "::fan:");
+        System.out.println(":fan::" + id + "::fan:");
         System.out.println(":fan::fan::fan:");
     }
-
 }
