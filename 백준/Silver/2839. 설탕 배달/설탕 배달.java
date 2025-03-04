152c243f1cd06d785848c4e72d4989f2430a7fa0
Jihoon Gweon <kjh8673a@naver.com>
2025-03-04 17:48:02 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,38 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        if (n >= 3) {
+            dp[3] = 1;
+        }
+        if (n >= 5) {
+            dp[5] = 1;
+        }
 
-        // 5킬로그램 봉지 개수
-        int five = n / 5;
-
-        // 3킬로그램 봉지 개수
-        int three = (n % 5) / 3;
-
-
-        // n을 5로 나눈 나머지가 3으로 나누어 떨어지면 그게 베스트
-        // 3으로 나누어떨어지지 않으면...
-        if((n % 5) % 3 != 0) {
-            // 5킬로그램 봉지를 최대에서 하나씩 줄여가면서 3으로 나눠본다
-            for(int i = n / 5; i >= 0; i--) {
-                if((n-5*i) % 3 == 0) {
-                    five = i;
-                    three = (n-5*i) / 3;
-                    break;
-                }else {
-                    five = 0;
-                    three = -1;
-                }
+        for (int i = 6; i <= n; i++) {
+            if (dp[i - 5] != 0 && dp[i - 3] != 0) {
+                dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
+            } else if (dp[i - 5] != 0) {
+                dp[i] = dp[i - 5] + 1;
+            } else if (dp[i - 3] != 0) {
+                dp[i] = dp[i - 3] + 1;
             }
         }
 
-        System.out.println(five + three);
-
+        System.out.println(dp[n] == 0 ? -1 : dp[n]);
     }
-
 }
