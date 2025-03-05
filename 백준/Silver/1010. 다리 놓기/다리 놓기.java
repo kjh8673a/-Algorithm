679b52f2d24de91ab5915f40964d5955aae0c19f
Jihoon Gweon <kjh8673a@naver.com>
2025-03-05 17:47:14 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,42 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int t = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < t; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            
-            // 팩토리얼을 계산해야 되므로 값이 엄청나게 커질거 대비하여 BigInteger
-            BigInteger n = new BigInteger(st.nextToken());
-            BigInteger m = new BigInteger(st.nextToken());
-            
-            // n <= m 이므로 오른쪽 m개중에 n개를 선택해서 놓인 순서대로 왼쪽과 연결하면 끝
-            // nCr = n!/n!(n-r)!
-            BigInteger mFac = factorial(m);
-            BigInteger nFac = factorial(n);
-            BigInteger minusFac = factorial(m.subtract(n));
+        int[][] dp = new int[30][30];
+        for (int i = 0; i < 30; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+        for (int i = 2; i < 30; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
 
-            sb.append(mFac.divide(nFac.multiply(minusFac))).append("\n");
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int tc = 0; tc < t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(dp[m][n]).append("\n");
         }
 
         System.out.println(sb.toString());
-    }
-
-    // 팩토리얼 계산
-    private static BigInteger factorial(BigInteger num) {
-        BigInteger one = new BigInteger("1");
-        if(num.intValue() == 0 || num.intValue() == 1) {
-            return one;
-        }else{
-            return factorial(num.subtract(one)).multiply(num);
-        }
     }
 }
