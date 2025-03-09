00c6bf15cb190b1c1f440d5e35a90ce07d6ec635
Jihoon Gweon <kjh8673a@naver.com>
2025-03-09 15:02:49 +0900

    [Gold III] Title: 앱, Time: 148 ms, Memory: 18536 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -8,42 +8,32 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[] memory = new int[n + 1];
-        int[] cost = new int[n + 1];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++) {
-            memory[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int sum = 0;
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
-            sum += cost[i];
-        }
-
-        int[][] dp = new int[n + 1][sum + 1];
-        for (int i = 1; i < n + 1; i++) {
-
-            for (int j = 0; j < sum + 1; j++) {
-                if (cost[i] <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost[i]] + memory[i]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+        int[][] apps = new int[n + 1][2];
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < n + 1; j++) {
+                apps[j][i] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int answer = 0;
-        for (int i = 0; i < sum + 1; i++) {
-            if (dp[n][i] >= m) {
-                answer = i;
-                break;
+        int[][] dp = new int[n + 1][10001];
+        int answer = Integer.MAX_VALUE;
+        for (int i = 1; i < n + 1; i++) {
+            int memory = apps[i][0];
+            int cost = apps[i][1];
+            for (int j = 0; j < 10001; j++) {
+                if (cost > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + memory);
+                }
+
+                if (dp[i][j] >= m) {
+                    answer = Math.min(answer, j);
+                }
             }
         }
 
         System.out.println(answer);
     }
-
 }
