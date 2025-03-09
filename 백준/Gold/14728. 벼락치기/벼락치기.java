050b2d675af43856aa4c90c1d42d1148138c1484
Jihoon Gweon <kjh8673a@naver.com>
2025-03-09 14:38:45 +0900

    [Gold V] Title: 벼락치기, Time: 128 ms, Memory: 18320 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.java
+++ 백준/Gold/14728. 벼락치기/벼락치기.java
@@ -1,50 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class Node {
-        int time, score;
-
-        public Node(int time, int score) {
-            this.time = time;
-            this.score = score;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int T = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
 
-        Node[] list = new Node[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        int[][] subjects = new int[n + 1][2];
+        for (int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            list[i] = new Node(a, b);
+            subjects[i][0] = Integer.parseInt(st.nextToken());
+            subjects[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] dp = new int[N + 1][T + 1];
-        for (int i = 1; i < N + 1; i++) {
-            int time = list[i].time;
-            int score = list[i].score;
-
-            for (int j = 0; j < T + 1; j++) {
-                if (time <= j) {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - time] + score);
-                } else {
+        int[][] dp = new int[n + 1][t + 1];
+        for (int i = 1; i < n + 1; i++) {
+            int time = subjects[i][0];
+            int score = subjects[i][1];
+            for (int j = 1; j < t + 1; j++) {
+                if (time > j) {
                     dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - time] + score);
                 }
             }
         }
 
-        System.out.println(dp[N][T]);
-
+        System.out.println(dp[n][t]);
     }
-
 }
