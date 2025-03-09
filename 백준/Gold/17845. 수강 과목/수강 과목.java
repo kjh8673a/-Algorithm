2647cab148fad8ac8bee8ba4a5b9caa1cae23b5a
Jihoon Gweon <kjh8673a@naver.com>
2025-03-09 15:42:25 +0900

    [Gold V] Title: 수강 과목, Time: 188 ms, Memory: 52124 KB -BaekjoonHub

--- 백준/Gold/17845. 수강 과목/수강 과목.java
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -8,26 +8,26 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] subject = new int[k + 1][2];
+        int[][] subjects = new int[k + 1][2];
         for (int i = 1; i < k + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            subject[i][0] = Integer.parseInt(st.nextToken());
-            subject[i][1] = Integer.parseInt(st.nextToken());
+            subjects[i][0] = Integer.parseInt(st.nextToken());
+            subjects[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[n + 1];
+        int[][] dp = new int[k + 1][n + 1];
         for (int i = 1; i < k + 1; i++) {
-            int importance = subject[i][0];
-            int time = subject[i][1];
-
-            for (int j = n; j > 0; j--) {
-                if (time <= j) {
-                    dp[j] = Math.max(dp[j], dp[j - time] + importance);
+            int cost = subjects[i][1];
+            int value = subjects[i][0];
+            for (int j = 1; j < n + 1; j++) {
+                if (cost > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + value);
                 }
             }
         }
 
-        System.out.println(dp[n]);
+        System.out.println(dp[k][n]);
     }
-
 }
