53ea7f8b594cfb97ef24d6d51ecc41c388d34ce2
Jihoon Gweon <kjh8673a@naver.com>
2025-03-09 15:24:28 +0900

    [Silver II] Title: 안녕, Time: 100 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.java
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -1,43 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n + 1][2];
+        StringTokenizer st;
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < n + 1; j++) {
+                arr[j][i] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] dp = new int[n + 1][100];
+        for (int i = 1; i < n + 1; i++) {
+            int cost = arr[i][0];
+            int value = arr[i][1];
+            for (int j = 0; j < 100; j++) {
+                if (cost > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + value);
+                }
+            }
+        }
 
-		int N = Integer.parseInt(br.readLine());
-
-		int[] hp = new int[N + 1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			hp[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int[] joy = new int[N + 1];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			joy[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int[][] dp = new int[N + 1][100];
-
-		for (int i = 1; i < N + 1; i++) {
-			int cost = hp[i];
-			int value = joy[i];
-			for (int j = 0; j < 100; j++) {
-				if (cost <= j) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + value);
-				} else {
-					dp[i][j] = dp[i - 1][j];
-				}
-			}
-		}
-
-		System.out.println(dp[N][99]);
-
-	}
-
+        System.out.println(dp[n][99]);
+    }
 }
