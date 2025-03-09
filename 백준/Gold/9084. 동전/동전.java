277e5b71751f7099a63a7763f870e36acef6a111
Jihoon Gweon <kjh8673a@naver.com>
2025-03-09 15:16:11 +0900

    [Gold V] Title: 동전, Time: 116 ms, Memory: 15512 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,53 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-
-        int T = Integer.parseInt(br.readLine());
-
         StringTokenizer st;
-        for (int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < t; tc++) {
+            int n = Integer.parseInt(br.readLine());
 
-            int[] coins = new int[N + 1];
+            int[] coins = new int[n + 1];
             st = new StringTokenizer(br.readLine());
-            for (int i = 1; i < N + 1; i++) {
+            for (int i = 1; i < n + 1; i++) {
                 coins[i] = Integer.parseInt(st.nextToken());
             }
 
-            int M = Integer.parseInt(br.readLine());
+            int m = Integer.parseInt(br.readLine());
 
-            int[][] dp = new int[N + 1][M + 1];
-
-            for (int i = 1; i < N + 1; i++) {
-                int now = coins[i];
-                for(int j = 0; j < M + 1; j++) {
-                    if(now <= j) {
-                        int cnt = 0;
-                        while(now * cnt < j) {
-                            dp[i][j] += dp[i-1][j - now*cnt];
-                            cnt++;
-                        }
-                    }else {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                    if(j % now == 0) {
-                        dp[i][j]++;
+            int[][] dp = new int[n + 1][m + 1];
+            for (int i = 1; i < n + 1; i++) {
+                int coin = coins[i];
+                dp[i][0] = 1;
+                for (int j = 1; j < m + 1; j++) {
+                    if (coin > j) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - coin];
                     }
                 }
             }
-            
-            sb.append(dp[N][M]).append("\n");
 
+            sb.append(dp[n][m]).append("\n");
         }
 
         System.out.println(sb.toString());
-
     }
-
 }
