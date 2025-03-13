2aa4d92ab51f54231a93022cf8e497b50f03ba46
Jihoon Gweon <kjh8673a@naver.com>
2025-03-13 17:22:01 +0900

    [Bronze I] Title: XORXORXOR, Time: 136 ms, Memory: 15920 KB -BaekjoonHub

---
+++ 백준/Bronze/12833. XORXORXOR/XORXORXOR.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int answer = a ^ b;
+        for (int i = 1; i < c; i++) {
+            answer ^= b;
+        }
+
+        System.out.println(answer);
+    }
+}
