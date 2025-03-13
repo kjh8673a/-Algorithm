ef777bab1f7a752e75eb5e2e040205db323bd64a
Jihoon Gweon <kjh8673a@naver.com>
2025-03-13 21:06:15 +0900

    [Silver IV] Title: GCD 합, Time: 240 ms, Memory: 23168 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.java
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -1,53 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
-		int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int tc = 0; tc < t; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++) {
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
 
-		for(int i = 0; i < t; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+            long answer = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    answer += gcd(Math.max(numbers[i], numbers[j]), Math.min(numbers[i], numbers[j]));
+                }
+            }
 
-			int n = Integer.parseInt(st.nextToken());
+            sb.append(answer).append("\n");
+        }
 
-			// n개의 숫자를 배열에 넣고 정렬해준다
-			int[] arr = new int[n];
-			for(int j = 0; j < n; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
-			}
+        System.out.println(sb.toString());
+    }
 
-			Arrays.sort(arr);
-
-			// (0, 1)쌍부터 (n-2, n-1)쌍까지 모든 경우를 계산한다
-			long ans = 0;
-			for(int k = 0; k < n-1; k++) {
-				for(int l = k+1; l < n; l++) {
-					ans += hose(arr[l], arr[k]);
-				}
-			}
-
-			sb.append(ans).append("\n");
-		}		
-
-		System.out.println(sb.toString());
-
-	}
-
-	// 유클리드 호제법
-	// a를 b로 나눈 나머지를 r이라 하면 a와 b의 최대공약수는 b와 r의 최대공약수와 같다
-	// 과정을 반복하여 나머지가 0이 되었을 때 나누는 수가 a와 b의 최대공약수이다
-	public static int hose(int a, int b) {
-        if (a % b != 0) {
-            return hose(b, a % b);
-        } else {
+    private static int gcd(int a, int b) {
+        if (a % b == 0) {
             return b;
         }
+        return gcd(b, a % b);
     }
 
 }
