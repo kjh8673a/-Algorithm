1a5cfcbfe383e2fa72d5554dfb195c1c915968a3
Jihoon Gweon <kjh8673a@naver.com>
2025-03-18 20:04:58 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,42 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        long n = Integer.parseInt(st.nextToken());
-
-        long cnt5 = pow5(n);
-        long cnt2 = pow2(n);
-
-        System.out.println(Math.min(cnt5, cnt2));
-    }
-
-    public static long pow5(long num) {
-        int cnt = 0;
-
-        while (num >= 5) {
-            cnt += (num / 5);
-            num /= 5;
+        int answer = 0;
+        while (n >= 5) {
+            answer += n / 5;
+            n /= 5;
         }
 
-        return cnt;
-    }
-
-    public static long pow2(long num) {
-        int cnt = 0;
-
-        while (num >= 2) {
-            cnt += (num / 2);
-            num /= 2;
-        }
-
-        return cnt;
+        System.out.println(answer);
     }
 
 }
