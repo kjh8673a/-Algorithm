1122c6abe0c088f789462cd9257d48bad7f4beb4
Jihoon Gweon <kjh8673a@naver.com>
2025-03-19 13:33:18 +0900

    [Silver IV] Title: 수 찾기, Time: 532 ms, Memory: 48732 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,54 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        Set<Integer> numbers = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers.add(Integer.parseInt(st.nextToken()));
+        }
 
-		int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-		arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            sb.append(numbers.contains(Integer.parseInt(st.nextToken())) ? 1 : 0).append("\n");
+        }
 
-		Arrays.sort(arr);
-
-		int m = Integer.parseInt(br.readLine());
-
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < m; i++) {
-			int num = Integer.parseInt(st.nextToken());
-
-			sb.append(binarySearch(num, 0, n - 1)).append("\n");
-		}
-
-		System.out.println(sb.toString());
-
-	}
-
-	private static int binarySearch(int num, int low, int high) {
-		int mid;
-
-		if (low <= high) {
-			mid = (low + high) / 2;
-
-			if (num == arr[mid]) {
-				return 1;
-			} else if (num < arr[mid]) {
-				return binarySearch(num, low, mid - 1);
-			} else {
-				return binarySearch(num, mid + 1, high);
-			}
-		}
-		return 0;
-	}
+        System.out.println(sb.toString());
+    }
 
 }
