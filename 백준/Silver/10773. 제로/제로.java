b48560a0bb0c782b12a64cb9b69bbd46bd9e82e0
Jihoon Gweon <kjh8673a@naver.com>
2025-03-19 13:29:48 +0900

    [Silver IV] Title: 제로, Time: 184 ms, Memory: 22456 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,34 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
 
-        int K = Integer.parseInt(br.readLine());
+        int[] numbers = new int[k];
+        int idx = 0;
+        for (int i = 0; i < k; i++) {
+            int number = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
-
-        // 합을 담을 변수
-        int ans = 0;
-
-        for (int i = 0; i < K; i++) {
-            int now = Integer.parseInt(br.readLine());
-            if (now != 0) {
-                // 0이 아니면 stack에 넣어주고, ans에 더해준다
-                stack.push(now);
-                ans += now;
+            if (number == 0) {
+                numbers[--idx] = 0;
             } else {
-                // 0이면 stack에서 꺼내주고 ans에서 빼준다
-                ans -= stack.pop();
+                numbers[idx++] = number;
             }
         }
 
-        System.out.println(ans);
+        int answer = 0;
+        for (int i = 0; i < idx; i++) {
+            answer += numbers[i];
+        }
 
+        System.out.println(answer);
     }
 
 }
