8a70b0ad9cd20f3ce60a822cd46e0e649e151a2c
Jihoon Gweon <kjh8673a@naver.com>
2025-03-19 13:05:27 +0900

    [Bronze III] Title: 완전 세제곱, Time: 168 ms, Memory: 16500 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int a = 2; a <= 100; a++) {
+            for (int b = 2; b <= a; b++) {
+                for (int c = b; c <= a; c++) {
+                    for (int d = c; d <= a; d++) {
+                        if (a * a * a == b * b * b + c * c * c + d * d * d) {
+                            sb.append("Cube = " + a + ", Triple = (" + b + "," + c + "," + d + ")").append("\n");
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+
+}
