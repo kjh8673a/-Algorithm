7d737ad03105c7f136861fcd482e57c484496e45
Jihoon Gweon <kjh8673a@naver.com>
2025-04-27 13:46:02 +0900

    [Silver IV] Title: 간판, Time: 100 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/5534. 간판/간판.java
+++ 백준/Silver/5534. 간판/간판.java
@@ -5,28 +5,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String name = br.readLine();
+        char[] name = br.readLine().toCharArray();
 
-        int count = 0;
-        loop: for (int i = 0; i < n; i++) {
-            char[] board = br.readLine().toCharArray();
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
 
-            for (int j = 0; j < board.length; j++) {
-                if (board[j] == name.charAt(0)) {
-                    for (int dist = 1; dist <= board.length; dist++) {
-                        StringBuilder sb = new StringBuilder();
-                        for (int k = j; k < board.length && sb.length() < name.length(); k += dist) {
-                            sb.append(board[k]);
-                        }
-                        if (sb.toString().equals(name)) {
-                            count++;
-                            continue loop;
+            loop: for (int j = 0; j < s.length(); j++) {
+
+                if (s.charAt(j) == name[0]) {
+                    for (int k = j + 1; k < s.length(); k++) {
+
+                        if (s.charAt(k) == name[1]) {
+                            int jump = k - j;
+                            int idx = k + jump;
+                            for (int l = 2; l < name.length; l++) {
+                                if (idx < s.length() && s.charAt(idx) == name[l]) {
+                                    idx += jump;
+                                    if (l == name.length - 1) {
+                                        answer++;
+                                        break loop;
+                                    }
+                                } else {
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
             }
         }
 
-        System.out.println(count);
+        System.out.println(answer);
     }
+
 }
